56776718c09173e9bb02fb274f7763c6e486e343
John Doe <andrewcreym@gmail.com>
2019-09-17 16:22:21 +0700

    welcome page fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -61,8 +61,8 @@ class HomeController extends Controller
         $tasks = \App\Task::orderBy('created_at', 'DESC')->limit(5)->get();
         $posts = \App\Post::orderBy('created_at', 'DESC')->limit(3)->get();
         $tasksCount = \App\Task::all()->count();
-        $executorsCount = \App\User::where('type', 1)->count();
-        $clientsCount = \App\User::where('type', 2)->count();
+        $executorsCount = \App\User::where('type', 2)->count();
+        $clientsCount = \App\User::where('type', 1)->count();
 
         return view('welcome', array('tasks' => $tasks, 'posts' => $posts, 'tasksCount' => $tasksCount, 'executorsCount' => $executorsCount, 'clientsCount' => $clientsCount));
     }
