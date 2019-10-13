bb1a7df51cf9e44397486c34dcde014454f67c14
John Doe <andrewcreym@gmail.com>
2019-10-13 22:15:58 +0700

    notice storing fixes

--- database/migrations/2019_10_13_133117_change_notice_column_type_in_tasks_table.php
+++ database/migrations/2019_10_13_133117_change_notice_column_type_in_tasks_table.php
@@ -14,7 +14,7 @@ class ChangeNoticeColumnTypeInTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->json('notice')->change();
+            $table->json('notice')->nullable()->change();
         });
     }
 
